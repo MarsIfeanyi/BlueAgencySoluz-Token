39e9685a0c12527b2219ae3c40b03001a7ba5e2b
MarsIfeanyi <marschalice@gmail.com>
2023-02-27 13:42:59 +0100

    Initial Commit

---
+++ contracts/BlueAgencySoluz.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BlueAgencySoluz is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("BlueAgencySoluz", "BAS") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
