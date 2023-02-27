cb10daf8513b3babbca69c2af70466e3888a43ff
MarsIfeanyi <marschalice@gmail.com>
2023-02-27 13:43:37 +0100

    Initial Commit

---
+++ helper-hardhat-config.js
@@ -0,0 +1,20 @@
+const networkConfig = {
+  default: {
+    name: "hardhat",
+  },
+  31337: {
+    name: "localhost",
+  },
+  5: {
+    name: "goerli",
+  },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
+};
